9561dc2e885d8afcc7a1e2cb15eaa079ce9d02e4
Newgin Sam <newginsam@gmail.com>
2021-10-04 00:14:51 +0530

    adding PWA to web application

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,3 +19,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const sendMail = require('./mail');
+const enforce = require('express-sslify')
+
 app.use(express.json());
 app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -33,7 +36,9 @@ app.post('/api/mail', async (req, res) => {
     }
 })
 
-
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client', 'build', 'service-worker.js'))
+})
 
 app.use(express.static('client/build'));
 if (process.env.NODE_ENV === 'production') {
